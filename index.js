9e70b014a34962bad4217deca03f75bdf871854d
Suryansh Pajiala <63901097+Sunny9899@users.noreply.github.com>
2021-11-29 15:46:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -104,9 +104,62 @@ app.get("/jobs/wfh",async(req,res)=>{
 
 
 // 3 ALL NOTICE PERIOD ACCEPTABLE JOBS
+app.get("/jobs/notice",async(req,res)=>{
+    try{
+       const jobs=await Job.find({Notice:{$eq:"Yes"}}).lean().exec();
+       return res.status(201).send(jobs);
+    }
+    catch(e){
+      return res.status(500).json({status:e.message});
+    }
+})
+
+// 4(a) SORTING JOBS WITH RATING(low to high)
+app.get("/jobs/sortLH",async(req,res)=>{
+    try{
+       const jobs=await Job.find().sort({Rating:1}).lean().exec();
+       return res.status(201).send(jobs);
+    }
+    catch(e){
+      return res.status(500).json({status:e.message});
+    }
+})
 
 
+// 4(b) SORTING JOBS WITH RATING(high to low)
+app.get("/jobs/sortHL",async(req,res)=>{
+    try{
+       const jobs=await Job.find().sort({Rating:-1}).lean().exec();
+       return res.status(201).send(jobs);
+    }
+    catch(e){
+      return res.status(500).json({status:e.message});
+    }
+})
 
+
+// 5 DETAILS OF COMPANY
+app.get("/comps",async(req,res)=>{
+    try{
+       const comp=await Comp.find().lean().exec();
+       return res.status(201).send(comp);
+    }
+    catch(e){
+      return res.status(500).json({status:e.message});
+    }
+})
+
+
+// 6 COMPANY WITH MOST OPEN JOBS
+app.get("/comps/opnjob",async(req,res)=>{
+    try{
+       const comp=await Comp.find().sort({open_jobs:-1}).limit(1).lean().exec();
+       return res.status(201).send(comp);
+    }
+    catch(e){
+      return res.status(500).json({status:e.message});
+    }
+})
 
 
 
