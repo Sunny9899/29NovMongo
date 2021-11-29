3f71c97fe28df09b9717952525d255600ed28fda
Suryansh Pajiala <63901097+Sunny9899@users.noreply.github.com>
2021-11-29 14:50:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,24 @@ const connect=()=>{
     return mongoose.connect(" mongodb://127.0.0.1:27017/data");
 }
 
-const jobSchema=new.mongoose.Schema({
+
+
+const compSchema=new mongoose.Schema({
+        comp_Name:{type:String,required:true},
+        comp_Empnum:{type:Number,required:true},
+        open_jobs:{type:Number, required:true},
+        comp_optCost:{type:Number, required:true},
+},
+{
+  versionKey:false,
+  timestamps:true,
+}
+)
+
+const Comp=mongoose.model("comp",compSchema);
+
+
+const jobSchema=new mongoose.Schema({
     Skill:{type:String, required:true},
     City:{type:String, required:true},
     WFH:{type:String, required:true},
@@ -15,10 +32,9 @@ const jobSchema=new.mongoose.Schema({
     Rating:{type:Number,required:true},
 
     company_Id:{
-        comp_Name:{type:String,required:true},
-        comp_Empnum:{type:Number,required:true},
-        open_jobs:{type:Number, required:true},
-        comp_optCost:{type:Number, required:true},
+    type:mongoose.Schema.Types.ObjectId,
+    ref:"comp",
+    required:true,
     }
 },
 {
@@ -32,4 +48,44 @@ const Job=mongoose.model("job",jobSchema);
 
 const app=express();
 
-app.use
+app.use(express.json());
+
+
+//COMPANY
+
+//POST
+app.post("/comps",async(req,res)=>{
+    try{
+       const comp=Comp.create(req.body);
+       return res.status(201).send(comp);
+    }
+    catch(e){
+      return res.status(500).json({status:e.message});
+    }
+})
+
+
+
+
+//JOBS
+
+//POST
+app.post("/jobs",async(req,res)=>{
+    try{
+       const job=Job.create(req.body);
+       return res.status(201).send(job);
+    }
+    catch(e){
+      return res.status(500).json({status:e.message});
+    }
+})
+
+
+
+
+
+
+app.listen(1234,async function(){
+    await connect();
+    console.log("Listening on Port 1234");
+})
